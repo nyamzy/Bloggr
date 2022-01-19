a8d5c6fc13a725168a923dc1cb2ccc9accdb298e
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-19 11:55:35 +0300

    remove id parameter from random test

--- app/test_random.py
+++ app/test_random.py
@@ -7,7 +7,7 @@ class RandomQuotesTest(unittest.TestCase):
     Test the behaviour of the Random class
     '''
     def setUp(self):
-        self.new_quote = Random("JohnDoe", 1234, "An example of a quote")
+        self.new_quote = Random("JohnDoe", "An example of a quote")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_quote, Random))
