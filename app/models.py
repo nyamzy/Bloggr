d7dc39c675ec78ccae9bed82458bed8df457b28d
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-19 22:52:10 +0300

    add models to manage.py

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255), index = True)
     email = db.Column(db.String(255), unique = True, index = True)
     bio = db.Column(db.String(255))
-    profile_pic_pah = db.Column(db.String())
+    profile_pic_path = db.Column(db.String())
     pass_secure  = db.Column(db.String(255))
 
     @property
