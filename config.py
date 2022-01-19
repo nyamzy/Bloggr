323bd0c229057f404f5992c5c207b96ca2cf7765
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-19 23:45:34 +0300

    set up test configuration

--- config.py
+++ config.py
@@ -7,13 +7,18 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nyamzy:redemption@localhost/blog'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    
+
 class ProdConfig(Config):
     '''
     Production  configuration child class
     '''
     pass
 
+class TestConfig(Config):
+    '''
+    Test configuration child class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nyamzy:redemption@localhost/blog_test'
 
 class DevConfig(Config):
     '''
@@ -23,5 +28,6 @@ class DevConfig(Config):
 
 config_optins = {
     'development': DevConfig,
-    'production': ProdConfig
+    'production': ProdConfig,
+    'test': TestConfig
 }
