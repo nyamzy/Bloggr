0acc6dec2cceb2b926e048864f9118ef32bf8b94
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-19 11:34:30 +0300

    create test for getting random quotes

---
+++ app/test_random.py
@@ -0,0 +1,17 @@
+import unittest
+from models import random
+Random = random.Random
+
+class RandomQuotesTest(unittest.TestCase):
+    '''
+    Test the behaviour of the Random class
+    '''
+    def setUp(self):
+        self.new_quote = Random("JohnDoe", 1234, "An example of a quote")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_quote, Random))
+
+
+if __name__ == '__main__':
+    unittest.main()
