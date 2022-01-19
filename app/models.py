2b096f26aab53dd5376880ac6b57aef86151278a
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-19 16:42:44 +0300

    create the user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from . import db
+
 class Random:
     '''
     Random class to define Random Quotes oblects
@@ -5,6 +7,15 @@ class Random:
     def __init__(self, author, quote):
         self.author = author
         self.quote = quote
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
 
 class Blog:
     pass
