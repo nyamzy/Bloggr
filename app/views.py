af11fd7056d9c22ec8a34343a00346aebdab679b
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-19 12:40:10 +0300

    edit index view function to show random quotes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_quotes
 
 #Views
 @app.route('/')
@@ -7,6 +8,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    random_quotes = get_quotes()
+    print(random_quotes)
     title = "Blog"
-    return render_template('index.html', title = title)
-    
+    return render_template('index.html', title = title, random = random_quotes)
