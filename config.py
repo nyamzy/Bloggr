01f8e5fd0a69d5cf775d2196a891c43bcf85a7c1
nyamzy <john.nyamweya@student.moringaschool.com>
2022-01-20 11:17:29 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class ProdConfig(Config):
     '''
     Production  configuration child class
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     '''
